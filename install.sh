be4810f0db21c2dd3924643b03b6cc13ffba8c9c
Ms.programmer <78996423+msprogrammer2938@users.noreply.github.com>
2021-04-12 19:47:56 -0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/bash
+# This code write by (Ms.nope)
+# installing portscan
+printf '\033]2;Installing Portscan installing\a'
+clear
+echo "installing..."
+sleep 1
+echo "
+
+   ░█▀▀█ █▀▀█ █▀▀█ ▀▀█▀▀ 　 ░█▀▀▀█ █▀▀ █▀▀█ █▀▀▄ 
+   ░█▄▄█ █──█ █▄▄▀ ──█── 　 ─▀▀▀▄▄ █── █▄▄█ █──█ 
+   ░█─── ▀▀▀▀ ▀─▀▀ ──▀── 　 ░█▄▄▄█ ▀▀▀ ▀──▀ ▀──▀
+             (🅟🅞🅡🅣 🅢🅒🅐🅝)"
+echo ""
+echo "[*] Checking Internet Connection: "
+wget -q --tries=10 --timeout=20 --spider https://google.com
+if [[ $? -eq 0 ]]; then
+    echo ""
+    echo "Internet connected!"
+    echo ""
+    echo "Installing portscan package..."
+    echo ""
+    sleep 1
+    sudo apt install update
+    sudo apt install python3
+    sudo apt install python-pip
+    pip install --upgrade pip
+    chmod +x portscan
+    pip install requirments.txt
+    echo ""
+    echo "finish installing!"
+    echo ""
+    echo "
+          usage:
+                ./portscan"
+    exit
+else
+    echo "
+         internet disconnected!
+         please check your internet connection..."
+         sleep 1
+    exit
+fi       
